a6a9b35cd44d1e6d6ba949a9527afd5c4e7a96b6
not.iron.yuriy <not.iron.yuriy@inbox.ru>
2025-02-20 18:24:08 +0300

    Секция products (ul > li > productItem)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,60 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProductCard from '../components/ProductCard.vue'
+
+const items = [
+  {
+    price: 500,
+    title: 'Бразильские зерна',
+    descr: 'Кофе, который ты всегда готовишь сам так, как тебе нравится.',
+    img: '1.jpg',
+  },
+  {
+    price: 600,
+    title: 'Вьетнамские зерна',
+    descr: 'Кофе, который ты всегда готовишь сам так, как тебе нравится.',
+    img: '1.jpg',
+  },
+  {
+    price: 700,
+    title: 'Африканские зерна',
+    descr: 'Кофе, который ты всегда готовишь сам так, как тебе нравится.',
+    img: '1.jpg',
+  },
+]
+</script>
 
 <template>
-  <main>
-    <h1 class="title-h1">Главная страница</h1>
-  </main>
+  <!-- <h1 class="title-h1">Главная страница</h1> -->
+
+  <section class="products">
+    <h2 class="title-h2 products__title">Это Наше Лучшее Предложение</h2>
+    <p class="products__descr">Кофейня поможет вам рассказать аудитории о вашем бизнесе.</p>
+    <ul class="products__list">
+      <li v-for="(item, i) in items" :key="i" class="products__item">
+        <ProductCard :price="item.price" :title="item.title" :descr="item.descr" :img="item.img" />
+      </li>
+    </ul>
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.products {
+  padding: 140px 0;
+
+  &__title {
+    margin-bottom: 20px;
+  }
+  &__descr {
+    margin-bottom: 60px;
+    font-size: 18px;
+    line-height: 160%;
+    opacity: 0.7;
+  }
+
+  &__list {
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+  }
+}
+</style>

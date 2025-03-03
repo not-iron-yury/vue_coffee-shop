ee3f6350ba93087fecad9be877f05490f9db2773
not.iron.yuriy <not.iron.yuriy@inbox.ru>
2025-03-03 18:23:08 +0300

    productsStore (быстрый доступ к товару в массиве)
    Долго делал и переделывал. Ещё дольше думал, как избежать постоянное
    использование поиска индекса конкретного товара в массиве items (все товары).
    
    Индекс в коллекции items не будет соотвествовать id товара. А именно по id
    работает остальная логика - добавление товара в список избраннго и корзину.
    
    Соответсвнно нужно связать id товара и его индекс в полученном массиве.
    Последний, в будущем будет изменяться, в зависимости от заданной сортировки и фильтра.
    
    В итоге - коллекция map {id => index, id => index, id => index ...}
    через которую и будет происходить взаимодействие с карточками товаров.

--- src/stores/productsStore.ts
+++ src/stores/productsStore.ts
@@ -1,22 +1,34 @@
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
-import { url } from '../API'
-import type { IProduct } from '../inretfaces'
+import { url } from '@/API'
+import type { IProduct } from '@/inretfaces'
 
 export const useProductsStore = defineStore('products', () => {
   const items = ref<IProduct[]>([])
+  const productIndexMap = new Map<number, number>() // коллекция индексов товаров, для быстрого доступа в массиве items
 
   const itemsBest = computed(() => {
     return items.value.filter((product) => product.topSelling)
   })
 
   const getProducts = async (): Promise<void> => {
-    const res = await fetch(url + '/items')
-    if (!res.ok) {
-      throw new Error('При попытке получить список товаров возникла ошибка.')
+    try {
+      const res = await fetch(url + '/items')
+      if (!res.ok) {
+        throw new Error('При попытке получить список товаров возникла ошибка.')
+      }
+      items.value = await res.json()
+      createProducIndexes()
+    } catch (err) {
+      console.error(err)
     }
-    items.value = await res.json()
   }
 
-  return { items, itemsBest, getProducts }
+  const createProducIndexes = (): void => {
+    for (let i = 0; i < items.value.length; i++) {
+      productIndexMap.set(items.value[i].id, i)
+    }
+  }
+
+  return { items, productIndexMap, itemsBest, getProducts }
 })

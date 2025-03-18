a6f848a369a6b75b2a31ee8b6b0bf4f3730b68a6
not-iron-yury <yuriy.shpak@inbox.ru>
2025-03-18 19:25:07 +0300

    Копоненты ProductFilter и FilterCheckboxes
    Changes to be committed:
    	new file:   src/components/Filter/AppFilterCheckboxes.vue
    	new file:   src/components/Filter/AppProductFilter.vue

---
+++ src/components/Filter/AppFilterCheckboxes.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['onChange'])
+
+const handlerCheckboxes = (event: Event) => {
+  if (event.target instanceof HTMLInputElement && event.target.value !== null) {
+    emit('onChange', `${event.target.name}=${event.target.value}`)
+  }
+}
+</script>
+
+<template>
+  <div class="filter-item">
+    <h3 class="filter-item__title">{{ data.title }}</h3>
+    <div class="filter-item__checkbox-list">
+      <label v-for="(option, index) in data.options" :key="index">
+        <input type="checkbox" :value="option" :name="data.name" @change="handlerCheckboxes" />
+        {{ option }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.filter-item {
+  &__title {
+    margin-bottom: 10px;
+    font-size: 21px;
+  }
+  &__checkbox-list {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+}
+</style>
---
+++ src/components/Filter/AppProductFilter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useProductsStore } from '@/stores/productsStore'
+import AppCatalogFilterItem from './AppFilterCheckboxes.vue'
+
+const productsStore = useProductsStore()
+
+const optionsWeight = {
+  title: 'Вес упаковки',
+  name: 'weight',
+  options: [500, 750, 1000, 1500, 2000],
+}
+
+const handlerFilter = (type: string) => {
+  productsStore.addOrRemoveFilteringType(type)
+}
+</script>
+
+<template>
+  <div class="catalog-filter">
+    <app-catalog-filter-item :data="optionsWeight" @onChange="handlerFilter" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>

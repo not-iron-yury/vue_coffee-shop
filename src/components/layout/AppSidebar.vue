bb2bac9c65475f48c7938206ef47dc7970ba0d41
not-iron-yury <yuriy.shpak@inbox.ru>
2025-03-18 19:24:23 +0300

    Компонент AppSidebar (сырой)

---
+++ src/components/layout/AppSidebar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppCatalogFilter from '../Filter/AppProductFilter.vue'
+</script>
+
+<template>
+  <aside class="sidebar">
+    <app-catalog-filter />
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  margin-top: 100px;
+  padding: 0 15px 0 0;
+  max-width: 280px;
+  width: 100%;
+
+  border-right: 1px solid rgb(207, 207, 207);
+}
+</style>
